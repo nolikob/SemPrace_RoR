5b6908552ff7f839a04715ec03b6630d646ffa59
Jan Čepelák <cepelak.honza@gmail.com>
2017-11-03 00:24:10 +0100

    Created content for room schedule and added dynamic link generating for rooms **li**

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,18 @@ var buildings = document.querySelectorAll(".building");
 
 buildings.forEach(function(element) {
     var btn = element.querySelector(".btnShow");
-    var building = element.querySelector(".rooms")
+    var building = element.querySelector(".more-info")
     btn.addEventListener("click",function(){
         building.classList.toggle("hidden");
     })
+});
+
+var uls = document.querySelectorAll('.rooms');
+
+uls.forEach(function(element) {
+  var lis = element.querySelectorAll('li');
+  lis.forEach(function(li) {
+    var prevContent = li.innerHTML;
+    li.innerHTML += "<a href=\"rooms.html\"> Zobraz rozvrh </a>";
+  })
 });
